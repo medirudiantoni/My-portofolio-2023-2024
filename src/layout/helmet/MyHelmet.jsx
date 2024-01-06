1e9155968ec8a76d808d6103583536f6f38a22a6
medi_rudiant <medirudiantoni2@gmail.com>
2024-01-06 15:01:08 +0700

    penyesuaian helmet

--- src/layout/helmet/MyHelmet.jsx
+++ src/layout/helmet/MyHelmet.jsx
@@ -3,14 +3,34 @@ import { Helmet } from 'react-helmet'
 import mediUnida from '../../assets/img/me/medi-unida.png'
 
 const MyHelmet = () => {
-  const keywords = ['medi rudiant', 'medirudiant', 'Medi Rudiant', 'Oosthavent', 'lakaran', 'kota agung', 'medi rudiantoni', '081273624740', 'mahasiswa umy', 'portfolio', 'portofolio', 'website portfolio', 'website portofolio', 'front-end', 'frontend', 'frontend web developer', "Medi Rudiant's Portfolio"]
+  const keywords = [
+    'medi rudiant', 
+    'medirudiant', 
+    'Medi Rudiant', 
+    'Oosthavent', 
+    'lakaran', 
+    'kota agung', 
+    'mahasiswa umy', 
+    'portfolio', 
+    'portofolio', 
+    'website portfolio', 
+    'website portofolio', 
+    'front-end', 
+    'frontend', 
+    'frontend web developer', 
+    "Medi Rudiant's Portfolio",
+    'web development',
+    'graphic design', 
+  ];
+  
   return (
     <Helmet>
+      <meta charSet="utf-8" />
       <title>Medi Rudiant - Portfolio</title>
       <meta name="description" content="Hello, I am Medi Rudiant, a front-end web developer and graphic designer" />
       <meta name="keywords" content={keywords.join(', ')} />
-      <meta name="author" content='Medi Rudiant' />
-      <meta name='creator' content='Medi Rudiant' />
+      <meta name="author" content="Medi Rudiant" />
+      <meta name="creator" content="Medi Rudiant" />
       <meta property="og:title" content="Medi Rudiant" />
       <meta property="og:description" content="Hello, I am Medi Rudiant, a front-end web developer and graphic designer" />
       <meta property="og:image" content={mediUnida} />
@@ -20,6 +40,7 @@ const MyHelmet = () => {
       <meta name="twitter:description" content="Hello, I am Medi Rudiant, a front-end web developer and graphic designer" />
       <meta name="twitter:image" content={mediUnida} />
       <meta name="google-site-verification" content="haZKmIeEcueD5dg8qT3YlPVRLsz4Mr5crgt9DTVGSGI" />
+      <meta name="google-site-verification" content="dQkwiFeomc83bxYwZIi6W9qVXZ0RZ6wXWTs5fKjne58" />
     </Helmet>
   )
 }
